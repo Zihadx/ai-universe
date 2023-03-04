49362b87a802b34f71c5bd595db8e8fdc499dbf0
zihadx <blankzihad@gmail.com>
2023-03-04 10:18:46 +0600

    show more data

--- js/main.js
+++ js/main.js
@@ -1,45 +1,70 @@
 const loadData = () => {
-  const url = "https://openapi.programming-hero.com/api/ai/tools";
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
   fetch(url)
-  .then(res=>res.json())
-  .then(data=> displayData(data.data.tools))
+    .then((res) => res.json())
+    .then((data) => displayData(data.data.tools.slice(0,6)));
 };
-const displayData = cards =>{
-    console.log(cards);
-    const cardContainer =document.getElementById('card-container');
-    cards.forEach(card=>{
-        // console.log(card);
-        const cardDiv = document.createElement('div')
-        cardDiv.classList.add('col')
-        cardDiv.innerHTML= `
+const displayData = (cards) => {
+  // console.log(cards);
+  const cardContainer = document.getElementById("card-container");
+ 
+  cardContainer.innerHTML=''
+
+  cards.forEach((card) => {
+    // console.log(card);
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("col");
+    cardDiv.innerHTML = `
         
         <div class="card h-100">
         <img src="${card.image}" class="card-img-top h-100 p-2 rounded " alt="...">
         <div class="card-body">
           <h4 class="card-title">Features</h4>
-
-          <ol>
-          <li>${card.features[0]}</li>
-          <li>${card.features[1]}</li>
-          <li>${card.features[2]}</li>
-        </ol>
-
+         <ol>
+           <li>${card.features[0]}</li>
+           <li>${card.features[1]}</li>
+           <li>${card.features[2] ? card.features[2] : 'Very exiting'}</li>
+           <li>${card.features[3] ? card.features[3] : card.name}</li>
+         </ol>
         </div>
         <div class="card-footer d-flex justify-content-between align-items-center">
-        <div>
-        <h4 class="card-title">${card.name}</h4>
-        <p>  <i class="fa-duotone fa-calendar-days"></i> </p>
-        <p>${card.published_in}</p>
-        </div>
-        <div>
-        <button class="btn btn-danger">Details</button>
-        </div>
+         <div>
+           <h4 class="card-title">${card.name}</h4>
+           <p> <span><i class="fa-solid fa-calendar-days"></i></span> ${card.published_in}</p>
+         </div>
+         <div>
+           <button type="button" onclick="loadCardDetails()" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#cardDetailsModal"><i class="fa-solid fa-arrow-right"></i>
+           </button>
+         </div>
         </div>
       </div>
 
-        `
-        cardContainer.appendChild(cardDiv)
-    })
+        `;
+    cardContainer.appendChild(cardDiv);
+  });
+};
+loadData();
 
+const showAllData =()=>{
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayData(data.data.tools));
 }
-loadData()
+
+// modal style and function start
+
+const loadCardDetails = async id =>{
+  
+  const url = ` https://openapi.programming-hero.com/api/ai/tool/${id}`
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log(data)
+}
+// const showCardDetails = data =>{
+//   console.log(data)
+// }
+loadCardDetails()
+
+
+// modal style and function end
--- js/test.js
+++ js/test.js
@@ -3,3 +3,64 @@
 // Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
 
 // Single data Example: https://openapi.programming-hero.com/api/ai/tool/01
+
+/*
+ // show 6 card
+  const showAll = document.getElementById("show-all");
+  if (cards.length > 6) {
+    cards = cards.slice(0, 6);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
+*/
+// displayData----------->
+const showAll = document.getElementById("show-all");
+if (cards.length > 6) {
+  cards = cards.slice(0, 6);
+  showAll.classList.remove("d-none");
+} else {
+  showAll.classList.add("d-none");
+}
+
+
+
+toggleSpiner(false)
+
+document.getElementById('btn-search').addEventListener('click', function(){
+  // start loader
+  toggleSpiner(true);
+  const searchField = document.getElementById('search-field');
+  const searchtext = searchField.value;
+  loadData(searchtext);
+})
+
+const toggleSpiner = isLoading =>{
+  const loaderSpiner = document.getElementById('loader')
+  if(isLoading){
+    loaderSpiner.classList.remove('d-none')
+  }
+  else{
+    loaderSpiner.classList.add('d-none')
+  }
+}
+
+// show all 
+document.getElementById('btn-show-all').addEventListener('click', function(){
+
+  toggleSpiner(true);
+  const searchField = document.getElementById('search-field');
+  const searchtext = searchField.value;
+  loadData(searchtext);
+})
+
+
+cardContainer.innerText=''
+const showAll = document.getElementById("show-all");
+if (cards.length > 6) {
+cards = cards.slice(0, 6);
+showAll.classList.remove("d-none");
+} else {
+cards = cards.slice(0,12);
+showAll.classList.add("d-none");
+}
