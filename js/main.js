7848c75a677b359485e70358be9339e34a611937
zihadx <blankzihad@gmail.com>
2023-03-04 12:59:24 +0600

    modal first part some work complet

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ const displayData = (cards) => {
            <p> <span><i class="fa-solid fa-calendar-days"></i></span> ${card.published_in}</p>
          </div>
          <div>
-           <button type="button" onclick="loadCardDetails()" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#cardDetailsModal"><i class="fa-solid fa-arrow-right"></i>
+           <button type="button" onclick="loadCardDetails('${card.id}')" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#cardDetailsModal"><i class="fa-solid fa-arrow-right"></i>
            </button>
          </div>
         </div>
@@ -54,16 +54,59 @@ const showAllData =()=>{
 
 // modal style and function start
 
-const loadCardDetails = async id =>{
+const loadCardDetails = (id) =>{
   
   const url = ` https://openapi.programming-hero.com/api/ai/tool/${id}`
-  const res = await fetch(url)
-  const data = await res.json()
-  console.log(data)
+  fetch(url)
+  .then(res=>res.json())
+  .then(data=>showCardDetails(data))
 }
-// const showCardDetails = data =>{
-//   console.log(data)
-// }
+const showCardDetails = data =>{
+  console.log(data)
+  const detailsFirstPart = document.getElementById('details-first-part');
+  detailsFirstPart.innerHTML = `
+  
+  
+  <div>
+  <h4>${data.data.description} </h4>
+  <div class="container text-center">
+  <div class="row gap-2 mt-4">
+  <div class="col  text-center bg-white p-3 rounded">
+  <h5>${data.data.pricing ? data.data.pricing[0].price : " "}</h5>
+  <h5>${data.data.pricing ? data.data.pricing[0].plan : " "}</h5>
+  </div>
+  
+  <div class="col text-center bg-white p-3 rounded">
+  <h5>${data.data.pricing ? data.data.pricing[1].price : " "}</h5>
+  <h5>${data.data.pricing ? data.data.pricing[1].plan : " "}</h5>
+  </div>
+  
+  <div class="col text-center bg-white p-3 rounded">
+  <h5>${data.data.pricing ? data.data.pricing[2].price : " "}</h5>
+  <h5>${data.data.pricing ? data.data.pricing[2].plan : " "}</h5>
+  </div>
+  </div>
+  </div>
+
+  <div class="mt-4">
+  <div>
+  <h4>Features<h4>
+  <ul>
+    <li class="fs-6">${data.data.features ? data.data.features[1].feature_name : ""}</li>
+    <li class="fs-6">${data.data.features ? data.data.features[2].feature_name : ""}</li>
+    <li class="fs-6">${data.data.features ? data.data.features[3].feature_name : ""}</li>
+  </ul>
+  </div>
+  <div></div>
+  </div>
+  
+  
+  </div>
+  `
+
+
+
+}
 loadCardDetails()
 
 
